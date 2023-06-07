a640153a5bf1faa86eacf3b01d3f41933920cae3
Wiam IDRISSI KHAMLICHI <khamlichiwiam2000@gmail.com>
2023-06-07 16:06:37 +0100

    Adding candidate service unit tests
    Added unit tests for CandidateRepository, CandidateService.

---
+++ recruitment_demand/src/test/java/recruitment_demand/RecruitmentDemandRepositoryTest.java
@@ -0,0 +1,191 @@
+package recruitment_demand;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.support.TestPropertySourceUtils;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import recruitment_demand.domain.RecruitmentDemand;
+import recruitment_demand.domain.out.RecruitmentDemandRepository;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+
+@Testcontainers
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ContextConfiguration(initializers = RecruitmentDemandRepositoryTest.DataSourceInitializer.class)
+public class RecruitmentDemandRepositoryTest {
+
+    @Autowired
+    private RecruitmentDemandRepository recruitmentDemandRepository;
+
+    @Container
+    private static final PostgreSQLContainer<?> database = new PostgreSQLContainer<>("postgres:15.3");
+
+    @Test
+    @DisplayName("should retrieve all recruitment demands from the database")
+    public void shouldRetrieveAllRecruitmentDemands() {
+
+        //Create multiple recruitment demands
+        RecruitmentDemand recruitmentDemand1 = new RecruitmentDemand();
+        recruitmentDemand1.setPostTitle("Software Developer");
+        recruitmentDemand1.setPostDescription("Post Description");
+        recruitmentDemand1.setNumberOfProfiles(5);
+        recruitmentDemand1.setNumberOfYearsOfExperience(2);
+        recruitmentDemand1.setLevelOfStudies("Bac + 5");
+        recruitmentDemand1.setStatusOfDemand("En cours");
+        recruitmentDemand1.setDateOfDemand(new Date(2023, 5, 7));
+
+        //Create multiple recruitment demands
+        RecruitmentDemand recruitmentDemand2 = new RecruitmentDemand();
+        recruitmentDemand2.setPostTitle("Software Engineer");
+        recruitmentDemand2.setPostDescription("Post Description");
+        recruitmentDemand2.setNumberOfProfiles(1);
+        recruitmentDemand2.setNumberOfYearsOfExperience(1);
+        recruitmentDemand2.setLevelOfStudies("Bac + 5");
+        recruitmentDemand2.setStatusOfDemand("En cours");
+        recruitmentDemand2.setDateOfDemand(new Date(2023, 5, 7));
+
+        // Save the recruitment demands to the database
+        recruitmentDemandRepository.saveAll(List.of(recruitmentDemand1, recruitmentDemand2));
+
+        // Retrieve all recruitment demands from the database
+        List<RecruitmentDemand> allRecruitmentDemands = recruitmentDemandRepository.findAll();
+
+        // Assert that the list of recruitment demands contains the expected number of entries
+        Assertions.assertEquals(allRecruitmentDemands.size(), 2);
+    }
+
+    @Test
+    @DisplayName("should retrieve a recruitment demand from the database")
+    public void shouldRetrieveARecruitmentDemand() {
+
+        // Create a new recruitment demand
+        RecruitmentDemand recruitmentDemand = new RecruitmentDemand();
+        recruitmentDemand.setPostTitle("Software Developer");
+        recruitmentDemand.setPostDescription("Post Description");
+        recruitmentDemand.setNumberOfProfiles(5);
+        recruitmentDemand.setNumberOfYearsOfExperience(2);
+        recruitmentDemand.setLevelOfStudies("Bac + 5");
+        recruitmentDemand.setStatusOfDemand("En cours");
+        recruitmentDemand.setDateOfDemand(new Date(2023, 5, 7));
+
+        // Save the recruitment demand to the database
+        RecruitmentDemand savedRecruitmentDemand = recruitmentDemandRepository.save(recruitmentDemand);
+
+        // Retrieve the recruitmentDemand from the database
+        RecruitmentDemand retrieveRecruitmentDemand = recruitmentDemandRepository.findById(savedRecruitmentDemand.getId())
+                .orElse(null);
+
+        // Assert that the retrieved recruitment demand is not null
+        Assertions.assertNotNull(retrieveRecruitmentDemand);
+
+        // Assert that the retrieved recruitment demand has the same ID as the saved recruitment demand
+        Assertions.assertEquals(retrieveRecruitmentDemand.getId(), savedRecruitmentDemand.getId());
+    }
+
+    @Test
+    @DisplayName("should save a recruitment demand to the database")
+    public void shouldSaveARecruitmentDemand() {
+
+        // Create a new recruitmentDemand
+        RecruitmentDemand recruitmentDemand = new RecruitmentDemand();
+        recruitmentDemand.setPostTitle("Software Developer");
+        recruitmentDemand.setPostDescription("Post Description");
+        recruitmentDemand.setNumberOfProfiles(5);
+        recruitmentDemand.setNumberOfYearsOfExperience(2);
+        recruitmentDemand.setLevelOfStudies("Bac + 5");
+        recruitmentDemand.setStatusOfDemand("En cours");
+        recruitmentDemand.setDateOfDemand(new Date(2023, 5, 7));
+
+        // Save the recruitmentDemand to the database
+        RecruitmentDemand savedRecruitmentDemand = recruitmentDemandRepository.save(recruitmentDemand);
+
+        // Assert that the saved recruitmentDemand has the expected values
+        Assertions.assertEquals(savedRecruitmentDemand.getPostTitle(), "Software Developer");
+        Assertions.assertEquals(savedRecruitmentDemand.getPostDescription(), "Post Description");
+        Assertions.assertEquals(savedRecruitmentDemand.getNumberOfProfiles(), 5);
+        Assertions.assertEquals(savedRecruitmentDemand.getNumberOfYearsOfExperience(), 2);
+        Assertions.assertEquals(savedRecruitmentDemand.getLevelOfStudies(), "Bac + 5");
+        Assertions.assertEquals(savedRecruitmentDemand.getStatusOfDemand(), "En cours");
+        Assertions.assertEquals(savedRecruitmentDemand.getDateOfDemand(), new Date(2023, 5, 7));
+    }
+
+    @Test
+    @DisplayName("should update a recruitment demand in the database")
+    public void shouldUpdateARecruitmentDemand() {
+
+        // Create a new recruitmentDemand
+        RecruitmentDemand recruitmentDemand = new RecruitmentDemand();
+        recruitmentDemand.setPostTitle("Software Developer");
+        recruitmentDemand.setPostDescription("Post Description");
+        recruitmentDemand.setNumberOfProfiles(5);
+        recruitmentDemand.setNumberOfYearsOfExperience(2);
+        recruitmentDemand.setLevelOfStudies("Bac + 5");
+        recruitmentDemand.setStatusOfDemand("En cours");
+        recruitmentDemand.setDateOfDemand(new Date(2023, 5, 7));
+
+        // Save the recruitmentDemand to the database
+        RecruitmentDemand savedRecruitmentDemand = recruitmentDemandRepository.save(recruitmentDemand);
+
+        // Update the recruitmentDemand's address
+        savedRecruitmentDemand.setLevelOfStudies("Bac + 3");
+
+        // Save the updated recruitmentDemand to the database
+        RecruitmentDemand updatedRecruitmentDemand = recruitmentDemandRepository.save(savedRecruitmentDemand);
+
+        // Assert that the saved recruitmentDemand has the expected values
+        Assertions.assertEquals(updatedRecruitmentDemand.getLevelOfStudies(), "Bac + 3");
+    }
+
+    @Test
+    @DisplayName("should delete a recruitment demand from the database")
+    public void shouldDeleteARecruitmentDemand() {
+
+        // Create a new recruitmentDemand
+        RecruitmentDemand recruitmentDemand = new RecruitmentDemand();
+        recruitmentDemand.setPostTitle("Software Developer");
+        recruitmentDemand.setPostDescription("Post Description");
+        recruitmentDemand.setNumberOfProfiles(5);
+        recruitmentDemand.setNumberOfYearsOfExperience(2);
+        recruitmentDemand.setLevelOfStudies("Bac + 5");
+        recruitmentDemand.setStatusOfDemand("En cours");
+        recruitmentDemand.setDateOfDemand(new Date(2023, 5, 7));
+
+        // Save the recruitmentDemand to the database
+        RecruitmentDemand savedRecruitmentDemand = recruitmentDemandRepository.save(recruitmentDemand);
+
+        // Delete the recruitmentDemand from the database
+        recruitmentDemandRepository.delete(savedRecruitmentDemand);
+
+        // Try to retrieve the deleted recruitmentDemand from the database
+        RecruitmentDemand deletedRecruitmentDemand = recruitmentDemandRepository.findById(savedRecruitmentDemand.getId())
+                .orElse(null);
+
+        // Assert that the saved recruitmentDemand has the expected values
+        Assertions.assertNull(deletedRecruitmentDemand);
+    }
+
+    public static class DataSourceInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+        @Override
+        public void initialize(ConfigurableApplicationContext applicationContext) {
+            TestPropertySourceUtils.addInlinedPropertiesToEnvironment(
+                    applicationContext,
+                    "spring.datasource.url=" + database.getJdbcUrl(),
+                    "spring.datasource.username=" + database.getUsername(),
+                    "spring.datasource.password=" + database.getPassword()
+            );
+        }
+    }
+}
---
+++ recruitment_demand/src/test/java/recruitment_demand/RecruitmentDemandServiceTest.java
@@ -0,0 +1,143 @@
+package recruitment_demand;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import recruitment_demand.application.in.RecruitmentDemandRequest;
+import recruitment_demand.domain.RecruitmentDemand;
+import recruitment_demand.domain.RecruitmentDemandResponse;
+import recruitment_demand.domain.RecruitmentDemandService;
+import recruitment_demand.domain.out.RecruitmentDemandRepository;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+
+@ExtendWith(MockitoExtension.class)
+public class RecruitmentDemandServiceTest {
+
+    @Mock
+    private RecruitmentDemandRepository recruitmentDemandRepository;
+
+    @InjectMocks
+    private RecruitmentDemandService recruitmentDemandService;
+
+    @Test
+    @DisplayName("should return all recruitment demand")
+    public void shouldReturnAllRecruitmentDemand() {
+
+        List<RecruitmentDemand> recruitmentDemands = Arrays.asList(
+                new RecruitmentDemand(1L, "Software Developer", "Post Description", 5, 2, "Bac + 5", "En cours", new Date()),
+                new RecruitmentDemand(2L, "Software Engineer", "Post Description", 1, 2, "Bac + 2", "En cours", new Date())
+        );
+
+        Mockito.when(recruitmentDemandRepository.findAll()).thenReturn(recruitmentDemands);
+
+        List<RecruitmentDemandResponse> recruitmentDemandResponses = recruitmentDemandService.getAllRecruitmentDemands();
+
+        Assertions.assertEquals(recruitmentDemands.size(), recruitmentDemandResponses.size());
+    }
+
+    @Test
+    @DisplayName("should return a recruitment demand by ID")
+    public void shouldReturnRecruitmentDemandById() {
+
+        Long recruitmentDemandId = 1L;
+        RecruitmentDemand recruitmentDemand = new RecruitmentDemand(recruitmentDemandId, "Software Developer", "Post Description", 5, 2, "Bac + 5", "En cours", new Date());
+
+        Mockito.when(recruitmentDemandRepository.findById(recruitmentDemandId)).thenReturn(Optional.of(recruitmentDemand));
+
+        RecruitmentDemandResponse recruitmentDemandResponse = recruitmentDemandService.getRecruitmentDemandById(recruitmentDemandId);
+
+        Assertions.assertEquals(recruitmentDemand.getId(), recruitmentDemandResponse.getId());
+        Assertions.assertEquals(recruitmentDemand.getPostTitle(), recruitmentDemandResponse.getPostTitle());
+        Assertions.assertEquals(recruitmentDemand.getPostDescription(), recruitmentDemandResponse.getPostDescription());
+        Assertions.assertEquals(recruitmentDemand.getNumberOfProfiles(), recruitmentDemandResponse.getNumberOfProfiles());
+        Assertions.assertEquals(recruitmentDemand.getNumberOfYearsOfExperience(), recruitmentDemandResponse.getNumberOfYearsOfExperience());
+        Assertions.assertEquals(recruitmentDemand.getLevelOfStudies(), recruitmentDemandResponse.getLevelOfStudies());
+        Assertions.assertEquals(recruitmentDemand.getStatusOfDemand(), recruitmentDemandResponse.getStatusOfDemand());
+        Assertions.assertEquals(recruitmentDemand.getDateOfDemand(), recruitmentDemandResponse.getDateOfDemand());
+
+    }
+
+    @Test
+    @DisplayName("should save recruitment demand")
+    public void shouldSaveRecruitmentDemand() {
+
+        RecruitmentDemandRequest recruitmentDemandRequest = new RecruitmentDemandRequest();
+
+        recruitmentDemandRequest.setPostTitle("Post Title");
+        recruitmentDemandRequest.setPostDescription("Post Description");
+        recruitmentDemandRequest.setNumberOfProfiles(3);
+        recruitmentDemandRequest.setNumberOfYearsOfExperience(4);
+        recruitmentDemandRequest.setLevelOfStudies("Bac + 4");
+        recruitmentDemandRequest.setStatusOfDemand("Annulé");
+        recruitmentDemandRequest.setDateOfDemand(new Date());
+
+        RecruitmentDemand savedRecruitmentDemand = RecruitmentDemand.builder()
+                .postTitle(recruitmentDemandRequest.getPostTitle())
+                .postDescription(recruitmentDemandRequest.getPostDescription())
+                .numberOfProfiles(recruitmentDemandRequest.getNumberOfProfiles())
+                .numberOfYearsOfExperience(recruitmentDemandRequest.getNumberOfYearsOfExperience())
+                .levelOfStudies(recruitmentDemandRequest.getLevelOfStudies())
+                .statusOfDemand(recruitmentDemandRequest.getStatusOfDemand())
+                .dateOfDemand(recruitmentDemandRequest.getDateOfDemand())
+                .build();
+
+        Mockito.when(recruitmentDemandRepository.save(any(RecruitmentDemand.class))).thenReturn(savedRecruitmentDemand);
+
+        RecruitmentDemandResponse recruitmentDemand =recruitmentDemandService.createRecruitmentDemand(recruitmentDemandRequest);
+
+        Assertions.assertEquals(recruitmentDemand.getNumberOfProfiles(), recruitmentDemandRequest.getNumberOfProfiles());
+    }
+
+    @Test
+    @DisplayName("should update recruitment demand")
+    public void shouldUpdateRecruitmentDemand() {
+        Long recruitmentDemandId = 1L;
+        RecruitmentDemandRequest recruitmentDemandRequest = new RecruitmentDemandRequest();
+
+        recruitmentDemandRequest.setPostTitle("Title");
+        recruitmentDemandRequest.setPostDescription("Description");
+        recruitmentDemandRequest.setNumberOfProfiles(3);
+        recruitmentDemandRequest.setNumberOfYearsOfExperience(4);
+        recruitmentDemandRequest.setLevelOfStudies("Bac + 4");
+        recruitmentDemandRequest.setStatusOfDemand("Annulé");
+        recruitmentDemandRequest.setDateOfDemand(new Date());
+
+        RecruitmentDemand existingRecruitmentDemand = new RecruitmentDemand(recruitmentDemandId, "Old Title", "Old Description", 5, 2, "Bac + 5", "En cours", new Date());
+
+        Mockito.when(recruitmentDemandRepository.findById(recruitmentDemandId)).thenReturn(Optional.of(existingRecruitmentDemand));
+
+        RecruitmentDemandResponse updatedRecruitmentDemandResponse = recruitmentDemandService.updateRecruitmentDemand(recruitmentDemandId, recruitmentDemandRequest);
+
+        Assertions.assertEquals(existingRecruitmentDemand.getId(), updatedRecruitmentDemandResponse.getId());
+        Assertions.assertEquals(existingRecruitmentDemand.getPostTitle(), updatedRecruitmentDemandResponse.getPostTitle());
+        Assertions.assertEquals(existingRecruitmentDemand.getPostDescription(), updatedRecruitmentDemandResponse.getPostDescription());
+        Assertions.assertEquals(existingRecruitmentDemand.getNumberOfProfiles(), updatedRecruitmentDemandResponse.getNumberOfProfiles());
+        Assertions.assertEquals(existingRecruitmentDemand.getNumberOfYearsOfExperience(), updatedRecruitmentDemandResponse.getNumberOfYearsOfExperience());
+        Assertions.assertEquals(existingRecruitmentDemand.getLevelOfStudies(), updatedRecruitmentDemandResponse.getLevelOfStudies());
+        Assertions.assertEquals(existingRecruitmentDemand.getStatusOfDemand(), updatedRecruitmentDemandResponse.getStatusOfDemand());
+        Assertions.assertEquals(existingRecruitmentDemand.getDateOfDemand(), updatedRecruitmentDemandResponse.getDateOfDemand());
+
+    }
+
+    @Test
+    @DisplayName("should delete recruitment demand")
+    public void shouldDeleteRecruitmentDemand() {
+        Long recruitmentDemandId = 1L;
+        Mockito.when(recruitmentDemandRepository.existsById(recruitmentDemandId)).thenReturn(true);
+
+        recruitmentDemandService.deleteRecruitmentDemand(recruitmentDemandId);
+
+        Mockito.verify(recruitmentDemandRepository).deleteById(recruitmentDemandId);
+    }
+}
